5f452b28f33e99047a1bf27f6ade3f72aa45f96d
Julia von Abel <julia.von-abel@uni.kn>
2018-11-16 17:33:01 +0100

    add picture, menu.js, change html, javascript and vue file

---
+++ public/js/menu.js
@@ -0,0 +1,58 @@
+
+var burgerMenu = [
+  {
+    "name": "The German",
+    "ing1": "Cheddar",
+    "ing2": "Tomatoes",
+    "ing3": "Onions",
+    "ing4": "Iceberg lettuce",
+    "ing5": "Cucumber",
+    "ing6": "Beef",
+    "veg": false,
+    "img": "img/german.jpg"
+  },
+  {
+    "name": "The Danish",
+    "ing1": "Cheddar",
+    "ing2": "Tomatoes",
+    "ing3": "Onions",
+    "ing4": "Rucola",
+    "ing5": "Cucumber",
+    "ing6": "Halloumi",
+    "veg": true,
+    "img": "img/danish.jpg"
+  },
+  {
+    "name": "The Italian",
+    "ing1": "Cheddar",
+    "ing2": "Pepperoni",
+    "ing3": "Onions",
+    "ing4": "Iceberg lettuce",
+    "ing5": "Cucumber",
+    "ing6": "Beef",
+    "veg": false,
+    "img": "img/italian.jpg"
+  },
+  {
+    "name": "The American",
+    "ing1": "Cheddar",
+    "ing2": "Tomatoes",
+    "ing3": "Onions",
+    "ing4": "Iceberg lettuce",
+    "ing5": "Bacon",
+    "ing6": "Beef",
+    "veg": false,
+    "img": "img/american.jpg"
+  },
+  {
+    "name": "The Australian",
+    "ing1": "Cheddar",
+    "ing2": "Tomatoes",
+    "ing3": "Onions",
+    "ing4": "Lambs lettuce",
+    "ing5": "Cucumber",
+    "ing6": "Tofu",
+    "veg": true,
+    "img": "img/australian.jpg"
+  }
+]
