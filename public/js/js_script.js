60c586c1c3bdfc00d3cd5c369adc0314c9513c28
Julia von Abel <julia.von-abel@uni.kn>
2018-11-16 17:34:46 +0100

    change js and vue file

--- public/js/js_script.js
+++ public/js/js_script.js
@@ -1,22 +1,154 @@
 
-function MenuItem(name, kcal, gl, lac, veg) {
-    this.burgerName = function() {
-      return name;
-    };
-    this.kcal = function() {
-      return kcal;
-    };
-    this.gluten = gl;
-    this.lactose = lac;
-    this.vegetarian = veg;
+/**
+var german = document.getElementById("germanBurger");
+var danish = document.getElementById("danishBurger");
+var italian = document.getElementById("italianBurger");
+var american = document.getElementById("americanBurger");
+var australian = document.getElementById("australianBurger");
+
+var burgers = [german, danish, italian, american, australian];
+
+var germanName = document.getElementById("germanName");
+var danishName = document.getElementById("danishName");
+var italianName = document.getElementById("italianName");
+var americanName = document.getElementById("americanName");
+var australianName = document.getElementById("australianName");
+
+var burgerNames = [germanName, danishName, italianName, americanName, australianName];
+
+for (i in burgerMenu)
+{
+var img = document.createElement("img");
+img.src = burgerMenu[i].img;
+
+var src = burgers[i];
+src.appendChild(img);
+
+var listItemIng1 = document.createElement("li");
+var listItemIng2 = document.createElement("li");
+var listItemIng3 = document.createElement("li");
+var listItemIng4 = document.createElement("li");
+var listItemIng5 = document.createElement("li");
+var listItemIng6 = document.createElement("li");
+var listIng1 = document.createTextNode(burgerMenu[i].ing1);
+var listIng2 = document.createTextNode(burgerMenu[i].ing2);
+var listIng3 = document.createTextNode(burgerMenu[i].ing3);
+var listIng4 = document.createTextNode(burgerMenu[i].ing4);
+var listIng5 = document.createTextNode(burgerMenu[i].ing5);
+var listIng6 = document.createTextNode(burgerMenu[i].ing6);
+
+if (burgerMenu[i].veg == 'true') {
+burgerNames[i].innerHTML = burgerMenu[i].name + "  --- vegetarian";
+}
+else {
+burgerNames[i].innerHTML = burgerMenu[i].name;
 }
 
-var menu = new MenuItem('The German', '1200kCal', 'contains gluten', 'contains lactose' , 'is not vegetarian');
-var menu2 = new MenuItem('The Danish', '870kCal', 'contains gluten', 'contains lactose' , 'is vegetarian');
 
-console.log( menu.burgerName() );
-console.log( menu.kcal() );
-console.log(menu2.burgerName());
-console.log(menu2.kcal());
+for (k in burgers) {
+listItemIng1.appendChild(listIng1);
+listItemIng2.appendChild(listIng2);
+listItemIng3.appendChild(listIng3);
+listItemIng4.appendChild(listIng4);
+listItemIng5.appendChild(listIng5);
+listItemIng6.appendChild(listIng6);
+burgers[i].appendChild(listItemIng1);
+burgers[i].appendChild(listItemIng2);
+burgers[i].appendChild(listItemIng3);
+burgers[i].appendChild(listItemIng4);
+burgers[i].appendChild(listItemIng5);
+burgers[i].appendChild(listItemIng6);
+}
 
-//document.getElementById("whatever").innerHTML = "hallo ";
+}
+**/
+
+
+function returnOrder() {
+  // textfields
+  var name = document.getElementById("name").value;
+
+  // var street = document.getElementById("street").value;
+  // var housenumber = document.getElementById("housenumber").value;
+  // var postalcode = document.getElementById("postalcode").value;
+  // var city = document.getElementById("city").value;
+
+  var email = document.getElementById("email").value;
+
+  // radio button: gender
+  var gender = document.querySelector('input[name="gender"]:checked').value;
+
+  // checkboxes: allergies
+  if (document.getElementsByName("allergic")[0].checked == true && document.getElementsByName("allergic")[1].checked == true) {
+    var allergies = "Gluten-free, Lactose-free"; }
+    else if (document.getElementsByName("allergic")[1].checked == true) {
+      allergies = "Lactose-free";
+    }
+    else if (document.getElementsByName("allergic")[0].checked == true ) {
+      allergies = "Gluten-free";
+    }
+
+    // select field: payment
+
+      var payment = document.getElementById("payment").value;
+
+
+    //checkbox: burger selection
+    // not working. Why??
+    // var burgerSelection = [];
+    // for (var j=0; j<6; j++){
+    //   if(document.getElementsByName("burgertype")[j].checked == true){
+    //     burgerSelection.push(document.getElementsByName("burgertype")[j].value);
+    //   }
+    // }
+    var burgerSelection = [];
+    if(document.getElementsByName("burgertype")[0].checked == true){
+      burgerSelection.push(document.getElementsByName("burgertype")[0].value);
+    }
+    if(document.getElementsByName("burgertype")[1].checked == true){
+      burgerSelection.push(document.getElementsByName("burgertype")[1].value);
+    }
+    if(document.getElementsByName("burgertype")[2].checked == true){
+      burgerSelection.push(document.getElementsByName("burgertype")[2].value);
+    }
+    if(document.getElementsByName("burgertype")[3].checked == true){
+      burgerSelection.push(document.getElementsByName("burgertype")[3].value);
+    }
+    if(document.getElementsByName("burgertype")[4].checked == true){
+      burgerSelection.push(document.getElementsByName("burgertype")[4].value);
+    }
+    console.log(burgerSelection);
+
+
+    // var allBurgers = document.getElementsByName("burgertype");
+    // var burgerSelection = [];
+    // for (var i=0; i<6; i++){
+    // if (allBurgers[i].checked == true) {
+    //   burgerSelection += allBurgers[i].value;
+    //   }
+    // }
+
+    var order = [];
+    order.push(name);
+    // order.push(street);
+    // order.push(housenumber);
+    // order.push(postalcode);
+    // order.push(city);
+    order.push(email);
+    order.push(gender);
+    order.push(allergies);
+    order.push(payment);
+    order.push(burgerSelection);
+
+    return order;
+  }
+
+  // click button
+  // var sendOrder = document.getElementById("order");
+  //
+  // sendOrder.onclick = function () {
+  //   console.log( "Button clicked." );
+  //   console.log(document.getElementById("name").value);
+  //   console.log(document.querySelector('input[name="gender"]:checked').value);
+  //   console.log(document.getElementById("payment").value);
+  // }
--- public/js/vue_script.js
+++ public/js/vue_script.js
@@ -1,6 +1,142 @@
-var burger = new Vue({
-  el: '#whatever',
+'use strict';
+var socket = io();
+
+
+var vm = new Vue({
+  el: '#burgerselection',
   data: {
-    arbitraryVariableName: 'Välj en burgare'
+    title: 'Select your Burger ',
+    burgers: burgerMenu,
+    vegi: '---vegetarian'
   }
+
 })
+
+
+var vm2 = new Vue ({
+  el: '#customerinformation',
+  data: {
+    name: '',
+    // address: '',
+    email: '',
+    gender: '',
+    allergies: '',
+    payment: '',
+    burgers: '',
+    orders: {},
+    position: {}
+  },
+  created: function () {
+     // socket.on('initialize', function (data) {
+     //   this.orders = data.orders;
+     // }.bind(this));
+
+     // socket.on('currentQueue', function (data) {
+     //   this.orders = data.orders;
+     // }.bind(this));
+   },
+  methods:{
+    // returnOrder: function() {
+    //   var custInfo = returnOrder();
+    //   console.log(custInfo);
+    //   console.log('button clicked');
+    //   this.$data.name = 'Name: '+ custInfo[0];
+    //   // this.$data.address = 'Address: '+ custInfo[1] + ' ' + custInfo[2] + ', ' + custInfo[3] + ' ' + custInfo[4];
+    //   this.$data.email = 'Email: ' + custInfo[1];
+    //   this.$data.gender = 'Gender: ' + custInfo[2];
+    //   if (custInfo[3] == undefined) {
+    //     this.$data.allergies = 'Allergies: none'
+    //   }
+    //   else {
+    //     this.$data.allergies = 'Allergies: ' + custInfo[3];
+    //   }
+    //   this.$data.payment = 'Payment: ' +custInfo[4];
+    //   this.$data.burgers = 'Your order: '+ custInfo[5];
+    // },
+    getNext: function () {
+      // var lastOrder = Object.keys(this.orders).reduce(function (last, next) {
+      //   return Math.max(last, next);
+      // }, 0);
+      // return lastOrder + 1;
+       return(Math.floor(Math.random()*1000));
+
+    },
+    addOrder: function (event) {
+
+      var custInfo = returnOrder();
+        this.$data.name = 'Name: '+ custInfo[0];
+        this.$data.email = 'Email: ' + custInfo[1];
+        this.$data.gender = 'Gender: ' + custInfo[2];
+        if (custInfo[3] == undefined) {
+          this.$data.allergies = 'Allergies: none';
+          custInfo[3] = 'no allergies';
+        }
+        else {
+          this.$data.allergies = 'Allergies: ' + custInfo[3];
+        }
+        this.$data.payment = 'Payment: ' +custInfo[4];
+        this.$data.burgers = 'Your order: '+ custInfo[5];
+
+      var offset = {x: event.currentTarget.getBoundingClientRect().left,
+                    y: event.currentTarget.getBoundingClientRect().top};
+      socket.emit("addOrder", { orderId: this.getNext(),
+                                details: { x: this.position.x,
+                                           y: this.position.y },
+                                orderItems: [custInfo[5]],
+                                personalInfo: custInfo[0] + "( "+ custInfo[1]+ ", " + custInfo[2]+ ", " + custInfo[3]+ ", "+ custInfo[4] + " )"
+                              });
+    },
+    displayOrder: function (event) {
+      console.log("DEV map click");
+      var offset = {x: event.currentTarget.getBoundingClientRect().left,
+                    y: event.currentTarget.getBoundingClientRect().top};
+      this.position = { x: event.clientX - 10 - offset.x,
+                        y: event.clientY - 10 - offset.y }
+
+
+    }
+  }
+
+})
+
+// var vm3 = new Vue({
+//   el: '#customerinformation',
+//   data: {
+//     orders: {},
+//     position: {}
+//   },
+//   created: function () {
+//     socket.on('initialize', function (data) {
+//       this.orders = data.orders;
+//     }.bind(this));
+//
+//     socket.on('currentQueue', function (data) {
+//       this.orders = data.orders;
+//     }.bind(this));
+//   },
+//   methods: {
+//     getNext: function () {
+//       var lastOrder = Object.keys(this.orders).reduce(function (last, next) {
+//         return Math.max(last, next);
+//       }, 0);
+//       return lastOrder + 1;
+//     },
+//     addOrder: function (event) {
+//       var offset = {x: event.currentTarget.getBoundingClientRect().left,
+//                     y: event.currentTarget.getBoundingClientRect().top};
+//       socket.emit("addOrder", { orderId: this.getNext(),
+//                                 details: { x: event.clientX - 10 - offset.x,
+//                                            y: event.clientY - 10 - offset.y },
+//                                 orderItems: ["Beans", "Curry"]
+//                               });
+//     },
+//     displayOrder: function (event) {
+//       var offset = {x: event.currentTarget.getBoundingClientRect().left,
+//                     y: event.currentTarget.getBoundingClientRect().top};
+//     this.position = { x: event.clientX - 10 - offset.x,
+//                       y: event.clientY - 10 - offset.y }
+//
+//
+//     }
+//   }
+// });
